d95d7b4b507f8fbab7f56078d4b3bb73ba7274e0
Kangwon Lee <kangwon_fwd@naver.com>
2015-12-16 00:14:16 +0900

    read text of the workspace file

--- build/read_workspace.py
+++ build/read_workspace.py
@@ -7,6 +7,10 @@ tensorflow_root = os.path.abspath(os.pardir)
 def main():
     os.chdir(tensorflow_root)
 
+    workspace_filename = 'WORKSPACE'
+    workspace_file = open(workspace_file, 'r')
+    workspace_txt = workspace_file.read()
+
     os.chdir(current_dir)
 
 
